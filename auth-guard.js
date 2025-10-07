90596a033a041d4ab441494c877e405e507c33ad
Metin Can Gül <metincangul35@gmail.com>
2025-10-07 14:47:48 +0300

    Update

--- auth-guard.js
+++
@@ -1,6 +0,0 @@
-const authToken = localStorage.getItem('authToken');
-if (!authToken) {
-    console.error("Auth Guard: Token bulunamadı! Giriş sayfasına yönlendiriliyor.");
-    alert("Bu sayfayı görüntülemek için giriş yapmanız gerekmektedir.");
-    window.location.href = 'invlogin.html'; 
-}
